43284bf9cacc830a6e1e49ae6d84a970acd63c86
kostnerek <kostekmc@gmail.com>
2022-04-09 11:28:55 +0200

    disabled ability to deploy on all branches

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
+import env from "react-dotenv";
+
 function App() {
 	const [currentTime, setCurrentTime] = useState(0);
 
 	useEffect(() => {
-		fetch("/api/time")
+		fetch(env.API_URL + "/api/time")
 			.then((res) => res.json())
 			.then((data) => {
 				setCurrentTime(data.time);
 			});
 	}, []);
-
+	console.log(process.env);
 	return (
 		<>
 			<div>{currentTime}</div>
