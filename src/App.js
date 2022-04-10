a4e1139f91cabcac34b4b049a2b8942d8863e614
kostnerek <kostekmc@gmail.com>
2022-04-10 17:33:59 +0200

    added test call

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ function App() {
 	const [currentTime, setCurrentTime] = useState(0);
 
 	useEffect(() => {
-		fetch(env.API_URL + "/api/time")
+		fetch("/api/time")
 			.then((res) => res.json())
 			.then((data) => {
 				setCurrentTime(data.time);
 			});
 	}, []);
 	console.log(process.env);
+	const makeCall = () => {
+		fetch("/api/user/user2")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+			});
+	};
 	return (
 		<>
 			<div>Current time im [ms]: {currentTime}</div>
+			<button onClick={makeCall}>Make call</button>
 		</>
 	);
 }
