03994dcf0b8936d7c48a98ded609837ab5499e36
kostnerek <kostekmc@gmail.com>
2022-04-10 17:45:49 +0200

    fixed typos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React, { useState, useEffect } from "react";
-import env from "react-dotenv";
 
 function App() {
-	const [currentTime, setCurrentTime] = useState(0);
-
-	useEffect(() => {
-		fetch("/api/time")
-			.then((res) => res.json())
-			.then((data) => {
-				setCurrentTime(data.time);
-			});
-	}, []);
-	console.log(process.env);
 	const makeCall = () => {
 		fetch("/api/user/user2")
 			.then((res) => res.json())
@@ -21,7 +10,6 @@ function App() {
 	};
 	return (
 		<>
-			<div>Current time im [ms]: {currentTime}</div>
 			<button onClick={makeCall}>Make call</button>
 		</>
 	);
